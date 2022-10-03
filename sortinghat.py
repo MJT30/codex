303adfec30bbdd47eb5ced77b7d2da12543ab55a
mylest913 <mylest913@gmail.com>
2022-10-02 21:34:50 -0700

    Done for the night

--- sortinghat.py
+++ sortinghat.py
@@ -8,9 +8,14 @@ Thegood = 'The Good'
 Thegreat = 'The Great'
 Thewise = "The Wise"
 Thebold = 'The Bold'
+violin = 'The Violin'
+trumpet = 'The Trumpet'
+piano = 'The Piano'
+drum = 'The Drum'
 
 Q1 = int(input("Q1) Do you like Dawn or Dusk? : "))
-Q2 = int(input("When I'm dead, I want people to remmeber me as: "))
+Q2 = input("Q2) When I'm dead, I want people to remmeber me as: ")
+Q3 = input('Q3) Which kind of instrument most pleases your ear?: ')
 
 
 if Q1 == dawn:
@@ -28,3 +33,19 @@ elif Q2 == Thewise:
     print('Ravenclaw earns', count + 1, 'point')
 elif Q2 == Thebold:
     print('Gryffindor earns', count + 1, 'point')
+else:
+    print('Wront input')
+
+
+if Q3 == violin:
+    print('Slytherin gains '. count + 1, 'point')
+elif Q3 == trumpet:
+    print('Huffelputt gains ', count + 1, 'point')
+elif Q3 == piano:
+    print('Ravenclaw gains', count + 1, 'point')
+elif Q3 == drum:
+    print('Gryffindor gains' + 1, 'point')
+else:
+    print('Wrong input')
+
+print(count)
