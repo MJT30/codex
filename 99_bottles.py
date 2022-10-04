8d00efea0682ff104f28c61ae2eb60f7d6489395
mylest913 <mylest913@gmail.com>
2022-10-03 22:39:02 -0700

    99 bottles of beer done

---
+++ 99_bottles.py
@@ -0,0 +1,8 @@
+# To find the square you multiply the number by itself. For example, 4 * 4 = 16
+
+
+for x in range(99, 0, -1):
+    print(f"{x} of beer on the wall")
+    print(f"{x} bottles of beer")
+    print("Take one down, pass it around")
+    print(f"{x} bottles of beer on the wall")
---
+++ detention.py
@@ -0,0 +1,4 @@
+# For Loop.......very useful
+
+for x in range(101):
+    print("I will not use snapchat in class")
